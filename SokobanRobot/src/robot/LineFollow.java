8d947b05e20d3cfddd2ce4a17c637e04c97562df
Benedikt Wiese <benedikt-wiese@hotmail.de>
2017-11-14 12:08:40 +0100

    Line following without start calibration

--- SokobanRobot/src/robot/LineFollow.java
+++ SokobanRobot/src/robot/LineFollow.java
@@ -24,8 +24,6 @@ public class LineFollow {
 	static int lightValueMinFront = 100;
 	static int lightValueMaxFront = 0;
 	
-	static PIDController pid = new PIDController(0, 10);
-	
 	// Constants
 	static int speed = 200;
 	static final int blackPuffer = 4;
@@ -36,33 +34,33 @@ public class LineFollow {
 		rightMotor.setSpeed(speed);
 		leftMotor.setSpeed(speed);
 		initializeLightSensors();
-		startCalibration();
+		//startCalibration();
 		
-		LCD.drawString("Min Left:  " + lightLeft.getLow(), 0, 0);
-		LCD.drawString("Max Left:  " + lightLeft.getHigh(), 0, 1);
-		LCD.drawString("Min Right: " + lightRight.getLow(), 0, 2);
-		LCD.drawString("Max Right: " + lightRight.getHigh(), 0, 3);
-		LCD.drawString("Min Front: " + lightFront.getLow(), 0, 4);
-		LCD.drawString("Max Front: " + lightFront.getHigh(), 0, 5);
+//		LCD.drawString("Min Left:  " + lightLeft.getLow(), 0, 0);
+//		LCD.drawString("Max Left:  " + lightLeft.getHigh(), 0, 1);
+//		LCD.drawString("Min Right: " + lightRight.getLow(), 0, 2);
+//		LCD.drawString("Max Right: " + lightRight.getHigh(), 0, 3);
+//		LCD.drawString("Min Front: " + lightFront.getLow(), 0, 4);
+//		LCD.drawString("Max Front: " + lightFront.getHigh(), 0, 5);
+//		
+//		// Turn until black line is hit, so that robot is oriented correctly
+//		rightMotor.backward();
+//		leftMotor.forward();
+//		
+//		while(lightFront.getLightValue() > 60) { // Hardcoded Margin Zone
+//			//LCD.drawString("Val Front: " + lightFront.getLightValue() + "  ", 0, 6);
+//			try {
+//				Thread.sleep(10);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		leftMotor.stop(true);
+//		rightMotor.stop();
+//		//LCD.drawString("stop my code", 0, 7);
 		
-		// Turn until black line is hit, so that robot is oriented correctly
-		rightMotor.backward();
-		leftMotor.forward();
-		
-		while(lightFront.getLightValue() > 60) { // Hardcoded Margin Zone
-			//LCD.drawString("Val Front: " + lightFront.getLightValue() + "  ", 0, 6);
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		leftMotor.stop(true);
-		rightMotor.stop();
-		//LCD.drawString("stop my code", 0, 7);
-		
-		moveForward(1, false);
+		moveForward(3, false);
 		LCD.drawString("Moved forward 1 field!", 0, 7);
 		
 		while(true) {
@@ -110,7 +108,7 @@ public class LineFollow {
 		// find line
 		// follow line
 		// stop motors when line is hit
-		
+		calibrateLightSensors();
 		for(int i = 0; i < numOfFields; i++) {
 			
 			// get a bit forward to pass current line
@@ -128,6 +126,7 @@ public class LineFollow {
 			
 			while(lightFront.getLightValue() < 80)
 			{
+				calibrateLightSensors();
 				try {
 					Thread.sleep(5);
 				} catch (InterruptedException e) {
@@ -138,6 +137,7 @@ public class LineFollow {
 			}
 			
 			while(lightFront.getLightValue() > 60) { // Hardcoded Margin Zone
+				calibrateLightSensors();
 				try {
 					Thread.sleep(5);
 				} catch (InterruptedException e) {
@@ -147,7 +147,7 @@ public class LineFollow {
 				controlSpeed(lightLeft.getLightValue()-lightRight.getLightValue());
 			}
 		}
-		rightMotor.stop();
+		rightMotor.stop(true);
 		leftMotor.stop();
 	}
 	
@@ -155,16 +155,16 @@ public class LineFollow {
 	// positive difference should turn robot to the right
 	private static void controlSpeed(int difference) {
 		
-		int pidVal = pid.doPID(difference);
+//		int pidVal = pid.doPID(difference);
+//		
+//		//difference *= 2;
+//		
+//		if(pidVal > speed) 
+//			pidVal = speed;
+//		else if (pidVal < -speed)
+//			pidVal = -speed;
 		
-		//difference *= 2;
-		
-		if(pidVal > speed) 
-			pidVal = speed;
-		else if (pidVal < -speed)
-			pidVal = -speed;
-		
-		leftMotor.setSpeed(speed + pidVal);
-		rightMotor.setSpeed(speed - pidVal);
+		leftMotor.setSpeed(speed + difference);
+		rightMotor.setSpeed(speed - difference);
 	}
 }
